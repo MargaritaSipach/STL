868897afde1bcba04c26289795856f1606e12d15
Sipach <margo.sipach.1992@gamil.com>
2019-01-14 21:51:10 +0300

    Framework step 1

---
+++ Framework1/Framework/Driver/Driver.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace Framework.Driver
+{
+    class Driver
+    {
+        private static IWebDriver driver;
+        private static WebDriverWait wait;
+
+        private Driver() { }
+
+        public static IWebDriver GetInstance()
+        {
+            if (driver == null)
+            {
+                driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+                driver.Manage().Window.Size = new System.Drawing.Size(driver.Manage().Window.Size.Width / 2, driver.Manage().Window.Size.Height);
+                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(90));
+            }
+            return driver;
+        }
+
+        public static WebDriverWait GetWait()
+        {
+            return wait;
+        }
+
+        public static void CloseBrowser()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ Framework1/Framework/Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace Framework
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        private Steps.Steps steps = new Steps.Steps();
+
+        [SetUp]
+        public void SetupTest()
+        {
+            steps.InitBrowser();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            steps.CloseBrowser();
+        }
+
+        [Test]
+        public void TestEmptyDepartureCity()
+        {
+            steps.SetMainData("Москва", "", "2019-02-12", "2019-02-13");
+            Assert.AreEqual(steps.GetMainPageError(), "Не выбран аэропорт отправления/прибытия или введен некорректно.");
+        }
+
+    }
+}
